6b2b35050eb63bfc7e46ff1ba9474ff2cd5512e9
xetri <xetri@users.noreply.github.com>
2026-03-21 06:32:59 +0000

    deploy: 7a98a2fab26b1fc904103a1ca1fd224083bf715d

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"052f31d115eceda8cbff1b3481fcde4330c4ae
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1099706315" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "4092909198" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
